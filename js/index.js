d3fbae73b1904bc3199ba36ff1b686ba4634b976
Artem Tumin <arttumin@gmail.com>
2014-08-29 16:03:00 +0400

    fixes

--- js/index.js
+++ js/index.js
@@ -120,16 +120,12 @@ var App = (function(){
 	var $btnLog = $('.js-btn-signin');
 	var $btnCab = $('.js-btn-edit');
 	var $cabForm = $('.cabinet-user');
-	var $cabSave = $('.js-cab-save');
 
 	$btnCab.click( function(){
 		$cabForm.addClass('edit');
 		$(this).parent().find('input').removeAttr('disabled');
 	});
-	$cabSave.click( function(){
-		$cabForm.removeClass('edit').find('input').addAttr('disabled');
-	});
-
+	
 	$btnLog.click( function(){
 		Popup.show(5);
 	});
